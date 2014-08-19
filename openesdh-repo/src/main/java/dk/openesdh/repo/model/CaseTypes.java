8e754302f16372a3560c8c40dfb911f8f7c8108d
fhp <fhp@magenta.dk>
2014-08-19 14:30:49 +0200

    casetype definition lookup

--- openesdh-repo/src/main/java/dk/openesdh/repo/model/CaseTypes.java
+++ openesdh-repo/src/main/java/dk/openesdh/repo/model/CaseTypes.java
@@ -1,8 +1,15 @@
 package dk.openesdh.repo.model;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
 
 import org.alfresco.service.cmr.dictionary.DictionaryService;
+import org.alfresco.service.cmr.dictionary.ModelDefinition;
+import org.alfresco.service.cmr.dictionary.PropertyDefinition;
+import org.alfresco.service.cmr.dictionary.TypeDefinition;
+import org.alfresco.service.namespace.QName;
 import org.springframework.extensions.webscripts.AbstractWebScript;
 import org.springframework.extensions.webscripts.WebScriptException;
 import org.springframework.extensions.webscripts.WebScriptRequest;
@@ -19,18 +26,49 @@ public class CaseTypes extends AbstractWebScript {
     private DictionaryService dictionaryService;
 
 
+    public void setDictionaryService(DictionaryService dictionaryService) {
+        this.dictionaryService = dictionaryService;
+    }
+
     public void execute(WebScriptRequest req, WebScriptResponse res)
             throws IOException
     {
+        Collection<QName> caseTypes = dictionaryService.getSubTypes(OpenESDHModel.TYPE_CASE_BASE,true);
+
+        ModelDefinition modelDefinition = dictionaryService.getModel(QName.createQName(OpenESDHModel.CASE_URI, "caseModel"));
+
+        Collection<QName> properties = dictionaryService.getProperties(QName.createQName(OpenESDHModel.CASE_URI,"caseModel"));
+
+        TypeDefinition simpleCase = dictionaryService.getType(OpenESDHModel.TYPE_CASE_SIMPLE);
+
+
         try
         {
+
+
+            Iterator iterator = simpleCase.getProperties().entrySet().iterator();
+            Iterator it = simpleCase.getProperties().entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry pairs = (Map.Entry) it.next();
+                 System.out.println(pairs.getKey() + " = " + pairs.getValue());
+            }
+
+
+
             // build a json object
             JSONObject obj = new JSONObject();
-            // put some data on it
-            obj.put("Hello", "World");
-            // build a JSON string and send it back
-            String jsonString = obj.toString();
-            res.getWriter().write(jsonString);
+
+            for (QName c : properties) {
+
+                // System.out.println(c.getPrefixString());
+
+                // put some data on it
+                PropertyDefinition def = dictionaryService.getProperty(c);
+                obj.put("name", "property:" + c + "  " + "type:" + def);
+                // build a JSON string and send it back
+                String jsonString = obj.toString();
+                res.getWriter().write(jsonString);
+            }
         }
         catch(JSONException e)
         {
