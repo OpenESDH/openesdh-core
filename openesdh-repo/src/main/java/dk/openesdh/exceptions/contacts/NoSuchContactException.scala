9e527db98185a402e047a716effa7746ca267b03
Lanre Abiwon <lanre@magenta-aps.dk>
2015-02-06 16:47:09 +0100

    1 - Removal of the final scala code >:( 2 - Superficial code changes

--- openesdh-repo/src/main/java/dk/openesdh/exceptions/contacts/NoSuchContactException.scala
+++
@@ -1,9 +0,0 @@
-package dk.openesdh.exceptions.contacts
-
-import org.alfresco.error.AlfrescoRuntimeException
-
-/**
- * @author Lanre Abiwon.
- */
-@SerialVersionUID(-351467831095433997L)
-class NoSuchContactException(errorMessage:String) extends AlfrescoRuntimeException(errorMessage) {}
