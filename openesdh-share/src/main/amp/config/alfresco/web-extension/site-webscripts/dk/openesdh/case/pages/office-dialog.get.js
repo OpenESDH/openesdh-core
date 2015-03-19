8a04641e9de16a9eceec3e9ad02f0141311a01f8
Torben N. Rasmussen <Torben.Rasmussen@visma.com>
2015-03-19 14:46:54 +0100

    Put dialog in a ClassicWindow widget

--- openesdh-share/src/main/amp/config/alfresco/web-extension/site-webscripts/dk/openesdh/case/pages/office-dialog.get.js
+++ openesdh-share/src/main/amp/config/alfresco/web-extension/site-webscripts/dk/openesdh/case/pages/office-dialog.get.js
@@ -1,14 +1,30 @@
 model.jsonModel = {
     widgets: [
         {
-            id: "officeForm",
-            name: "alfresco/forms/Form",
+            name: "alfresco/layout/ClassicWindow",
             config: {
-                okButtonPublishTopic: "OFFICE_INTEGRATION_OK",
-                okButtonPublishGlobal: true,
-                cancelButtonPublishTopic: "OFFICE_INTEGRATION_CANCEL",
-                cancelButtonPublishGlobal: true,
+                title: "Office Dialog",
                 widgets: [
+                    {
+                        name: "alfresco/layout/VerticalWidgets",
+                        config: {
+                            fieldId: "2f83a87e-536d-4f04-83e8-07b0d4a353c3",
+                            widgets: [
+                                {
+                                    id: "officeForm",
+                                    name: "alfresco/forms/Form",
+                                    config: {
+                                        okButtonPublishTopic: "OFFICE_INTEGRATION_OK",
+                                        okButtonPublishGlobal: true,
+                                        cancelButtonPublishTopic: "OFFICE_INTEGRATION_CANCEL",
+                                        cancelButtonPublishGlobal: true,
+                                        widgets: [
+                                        ]
+                                    }
+                                }
+                            ]
+                        }
+                    }
                 ]
             }
         }
