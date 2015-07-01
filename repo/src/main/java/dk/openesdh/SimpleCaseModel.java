5fbfd1a09b7ed3f09523a4112253aa3654e4387c
Jurij Rudinskij <Jurij.Rudinskij@visma.com>
2015-07-01 15:09:48 +0300

    Simple case model constants corrections to correspond ones defined in simple_case-model.xml

--- repo/src/main/java/dk/openesdh/SimpleCaseModel.java
+++ repo/src/main/java/dk/openesdh/SimpleCaseModel.java
@@ -6,9 +6,10 @@ import org.alfresco.service.namespace.QName;
  * @author Lanre Abiwon.
  */
 public interface SimpleCaseModel {
-    public static final String SIMPLE_CASE_URI = "http://openesdh.dk/model/case/simple/1.1/";
+    public static final String SIMPLE_CASE_URI = "http://openesdh.dk/model/case/simple/1.0/";
     public static final String SIMPLE_CASE_PREFIX = "caseSimple";
     public static final String TYPE_SIMPLE_NAME = "simple";
+    public static final String TYPE_CASE = "case";
 
     /**
      * Models
@@ -18,5 +19,5 @@ public interface SimpleCaseModel {
     /**
      * Types
      */
-    public static final QName TYPE_CASE_SIMPLE = QName.createQName(SIMPLE_CASE_URI, TYPE_SIMPLE_NAME);
+    public static final QName TYPE_CASE_SIMPLE = QName.createQName(SIMPLE_CASE_URI, TYPE_CASE);
 }
