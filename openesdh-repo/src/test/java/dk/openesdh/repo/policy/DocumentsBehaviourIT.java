701b749422b0a3c803c37bff69dc72c0478c1d5c
Lanre Abiwon <lanre@magenta-aps.dk>
2015-07-22 09:59:54 +0200

    refactored to remove the org.hamcrest.core  methods

--- openesdh-repo/src/test/java/dk/openesdh/repo/policy/DocumentsBehaviourIT.java
+++ openesdh-repo/src/test/java/dk/openesdh/repo/policy/DocumentsBehaviourIT.java
@@ -1,14 +1,12 @@
 package dk.openesdh.repo.policy;
 
-import static org.hamcrest.core.IsCollectionContaining.hasItem;
-
-import java.io.Serializable;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
+import com.google.common.base.Strings;
+import com.tradeshift.test.remote.Remote;
+import com.tradeshift.test.remote.RemoteTestRunner;
+import dk.openesdh.repo.helper.CaseDocumentTestHelper;
+import dk.openesdh.repo.helper.CaseHelper;
+import dk.openesdh.repo.model.OpenESDHModel;
+import dk.openesdh.repo.services.cases.CaseService;
 import org.alfresco.model.ContentModel;
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
 import org.alfresco.repo.transaction.RetryingTransactionHelper;
@@ -20,7 +18,6 @@ import org.alfresco.service.namespace.NamespaceService;
 import org.alfresco.service.namespace.QName;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.tika.mime.MimeTypes;
-import org.hamcrest.MatcherAssert;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -31,14 +28,9 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.google.common.base.Strings;
-import com.tradeshift.test.remote.Remote;
-import com.tradeshift.test.remote.RemoteTestRunner;
-
-import dk.openesdh.repo.helper.CaseDocumentTestHelper;
-import dk.openesdh.repo.helper.CaseHelper;
-import dk.openesdh.repo.model.OpenESDHModel;
-import dk.openesdh.repo.services.cases.CaseService;
+import java.io.Serializable;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RunWith(RemoteTestRunner.class)
 @Remote(runnerClass = SpringJUnit4ClassRunner.class)
@@ -74,10 +66,8 @@ public class DocumentsBehaviourIT {
 
     @Before
     public void setUp() throws Exception {
-
         // TODO: All of this could have been done only once
         AuthenticationUtil.setFullyAuthenticatedUser(AuthenticationUtil.getAdminUserName());
-
         testFolder = docTestHelper.createFolder(TEST_FOLDER_NAME);
         testCase1 = docTestHelper.createCaseBehaviourOn(TEST_CASE_NAME1, testFolder, CaseHelper.DEFAULT_USERNAME);
         testCase1DocumentsFolder = caseService.getDocumentsFolder(testCase1);
@@ -86,17 +76,15 @@ public class DocumentsBehaviourIT {
     @After
     public void tearDown() throws Exception {
         AuthenticationUtil.setFullyAuthenticatedUser(AuthenticationUtil.getAdminUserName());
-        List<NodeRef> folders = Arrays.asList(new NodeRef[] { testFolder });
-        List<NodeRef> cases = Arrays.asList(new NodeRef[] { testCase1 });
-        List<String> users = Arrays.asList(new String[] { CaseHelper.DEFAULT_USERNAME });
+        List<NodeRef> folders = Arrays.asList(testFolder);
+        List<NodeRef> cases = Arrays.asList(testCase1);
+        List<String> users = Arrays.asList(CaseHelper.DEFAULT_USERNAME);
         docTestHelper.removeNodesAndDeleteUsersInTransaction(folders, cases, users);
     }
 
     @Test
     public void shouldAddDocumentToCase() {
-
         addDocumentToCase();
-
         Assert.assertNotNull("The node ref of the added document shouldn't be null", testAddDocument);
 
         String resultFileName = (String) nodeService.getProperty(testAddDocument, ContentModel.PROP_NAME);
@@ -105,27 +93,25 @@ public class DocumentsBehaviourIT {
                 Strings.isNullOrEmpty(resultExtension));
         Assert.assertEquals("Wrong extension of the added document", "txt", resultExtension);
 
-        Map<String, NodeRef> caseDocsFolderContent = nodeService
-                .getChildAssocs(testCase1DocumentsFolder)
-                .stream()
-                .collect(
-                        Collectors.<ChildAssociationRef, String, NodeRef> toMap(assoc -> (String) nodeService
-                                .getProperty(assoc.getChildRef(), ContentModel.PROP_NAME), assoc -> assoc
-                                .getChildRef()));
+        Map<String, NodeRef> caseDocsFolderContent = nodeService.getChildAssocs(testCase1DocumentsFolder).stream().collect(
+                        Collectors.<ChildAssociationRef, String, NodeRef> toMap(
+                                assoc -> (String) nodeService.getProperty(assoc.getChildRef(),
+                                        ContentModel.PROP_NAME), assoc -> assoc.getChildRef()
+                        ) );
 
         Assert.assertFalse(
                 "The added document should be put to the document record folder NOT to the case documents folder",
                 caseDocsFolderContent.values().contains(testAddDocument));
 
-        MatcherAssert.assertThat("The case documents folder should contain the added document record folder",
-                caseDocsFolderContent.keySet(), hasItem(TEST_ADD_DOCUMENT_NAME));
+        Assert.assertTrue("The case documents folder should contain the added document record folder",
+                hasKey(caseDocsFolderContent.keySet(), TEST_ADD_DOCUMENT_NAME));
 
         NodeRef addedDocRecordFolder = caseDocsFolderContent.get(TEST_ADD_DOCUMENT_NAME);
         List<NodeRef> addedDocRecordFolderContent = nodeService.getChildAssocs(addedDocRecordFolder).stream()
-                .map(docAssoc -> docAssoc.getChildRef()).collect(Collectors.toList());
+                .map(ChildAssociationRef::getChildRef).collect(Collectors.toList());
 
-        MatcherAssert.assertThat("The document record folder should contain the added document",
-                addedDocRecordFolderContent, hasItem(testAddDocument));
+        Assert.assertTrue("The document record folder should contain the added document",
+                addedDocRecordFolderContent.contains(testAddDocument));
     }
 
     private void addDocumentToCase() {
@@ -143,5 +129,9 @@ public class DocumentsBehaviourIT {
                         ContentModel.ASSOC_CONTAINS,
                         QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, TEST_ADD_DOCUMENT_NAME),
                         ContentModel.TYPE_CONTENT, properties).getChildRef()));
+    }
+
+    public boolean hasKey(Set<String> keySet, String interest){
+        return keySet.contains(interest);
     }
 }
