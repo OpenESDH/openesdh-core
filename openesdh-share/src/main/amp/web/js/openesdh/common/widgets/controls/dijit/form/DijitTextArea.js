8187943a46b9830e680fdc8a18470d5dd53563f4
Raimond Valaisis <raimond.valaisis@visma.com>
2015-08-04 16:12:24 +0300

    OPENE-184 Removed onInput from DijitTextArea

--- openesdh-share/src/main/amp/web/js/openesdh/common/widgets/controls/dijit/form/DijitTextArea.js
+++ openesdh-share/src/main/amp/web/js/openesdh/common/widgets/controls/dijit/form/DijitTextArea.js
@@ -13,16 +13,6 @@ define(["dojo/_base/declare",
             
             constructor: function(){
                 this.inherited(arguments);
-            },
-            
-            _onInput: function(/*Event*/ evt){
-    			// summary:
-    			//		Called AFTER the input event has happened
-    			//		See if the placeHolder text should be removed or added while editing.
-    			this.inherited(arguments);
-    			//this._updatePlaceHolder();
-    			
-    			console.log('wsup');
-    		},
+            }
         });
     });
