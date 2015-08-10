7a033b1e4cf54d53b6ceb2b88b8fc5eb8e01b649
Raimond Valaisis <raimond.valaisis@visma.com>
2015-08-10 16:31:13 +0300

    OPENE-189 Case history widget made with AlfList

--- openesdh-share/src/main/amp/config/alfresco/web-extension/site-webscripts/dk/openesdh/case/pages/history.get.js
+++ openesdh-share/src/main/amp/config/alfresco/web-extension/site-webscripts/dk/openesdh/case/pages/history.get.js
@@ -3,7 +3,7 @@
 var caseId = url.templateArgs.caseId;
 var caseNodeRef = getCaseNodeRefFromId(caseId);
 
-model.jsonModel = {
+/*model.jsonModel = {
         services: [
             "alfresco/services/CrudService"
         ],
@@ -28,4 +28,149 @@ model.jsonModel = {
                  }]
             }
         }]
-};
+};*/
+
+model.jsonModel = {
+	    services: [
+	        "alfresco/services/CrudService"
+	    ],
+	    widgets: [{
+
+
+	            name: "alfresco/layout/BootstrapContainer",
+	            config: {
+
+	                widgets: [{
+	                    id: "CASE_HISTORY_DASHLET",
+	                    name: "alfresco/dashlets/Dashlet",
+	                    config: {
+	                        title: msg.get("header.title"),
+	                        widgetsForBody: [{
+	                                name: "alfresco/lists/AlfSortablePaginatedList",
+	                                config: {
+	                                    loadDataPublishTopic: "ALF_CRUD_GET_ALL",
+	                                    loadDataPublishPayload: {
+	                                        url: "api/openesdh/casehistory?nodeRef=" + caseNodeRef
+	                                    },
+	                                    itemsProperty: "",
+	                                    currentPageSize: 5,
+	                                    startIndexProperty: "paging.skipCount",
+	                                    totalResultsProperty: "paging.totalItems",
+	                                    widgets: [{
+	                                        name: "alfresco/lists/views/AlfListView",
+	                                        config: {
+	                                            additionalCssClasses: "bordered",
+	                                            widgetsForHeader: [{
+	                                                name: "alfresco/lists/views/layouts/HeaderCell",
+	                                                config: {
+	                                                    label: ""
+	                                                }
+	                                            }, {
+	                                                name: "alfresco/lists/views/layouts/HeaderCell",
+	                                                config: {
+	                                                    label: msg.get("casehistory.column.action"),
+	                                                }
+	                                            }, {
+	                                                name: "alfresco/lists/views/layouts/HeaderCell",
+	                                                config: {
+	                                                    label: msg.get("casehistory.column.type"),
+	                                                }
+	                                            }, {
+	                                                name: "alfresco/lists/views/layouts/HeaderCell",
+	                                                config: {
+	                                                    label: msg.get("casehistory.column.user"),
+	                                                }
+	                                            }, {
+	                                                name: "alfresco/lists/views/layouts/HeaderCell",
+	                                                config: {
+	                                                   label: msg.get("casehistory.column.time"),
+	                                                }
+	                                            }],
+	                                            widgets: [{
+	                                                name: "alfresco/lists/views/layouts/Row",
+	                                                config: {
+	                                                    widgets: [{
+	                                                        name: "alfresco/lists/views/layouts/Cell",
+	                                                        config: {
+	                                                            widgets: [{
+	                                                                config: {
+	                                                                    propertyToRender: "type"
+	                                                                }
+	                                                            }]
+	                                                        }
+	                                                    }, {
+	                                                        name: "alfresco/lists/views/layouts/Cell",
+	                                                        config: {
+	                                                            additionalCssClasses: "mediumpad",
+	                                                            widgets: [{
+	                                                                name: "alfresco/renderers/Property",
+	                                                                config: {
+	                                                                    propertyToRender: "action"
+	                                                                }
+	                                                            }]
+	                                                        }
+	                                                    }, {
+	                                                        name: "alfresco/lists/views/layouts/Cell",
+	                                                        config: {
+	                                                            additionalCssClasses: "mediumpad",
+	                                                            widgets: [{
+	                                                                name: "alfresco/renderers/Property",
+	                                                                config: {
+	                                                                    propertyToRender: "type"
+	                                                                }
+	                                                            }]
+	                                                        }
+	                                                    }, {
+	                                                        name: "alfresco/lists/views/layouts/Cell",
+	                                                        config: {
+	                                                            additionalCssClasses: "mediumpad",
+	                                                            widgets: [{
+	                                                                name: "alfresco/renderers/Property",
+	                                                                config: {
+	                                                                    propertyToRender: "user"
+	                                                                }
+	                                                            }]
+	                                                        }
+	                                                    }, {
+	                                                        name: "alfresco/lists/views/layouts/Cell",
+	                                                        config: {
+	                                                            additionalCssClasses: "mediumpad",
+	                                                            width: "200px",
+	                                                            widgets: [{
+	                                                                name: "alfresco/renderers/Date",
+	                                                                config: {
+	                                                                    simple: true,
+	                                                                    propertyToRender: "time"
+	                                                                }
+	                                                            }]
+	                                                        }
+	                                                    }]
+	                                                }
+	                                            }]
+	                                        }
+	                                    }]
+	                                }
+	                            }, {
+	                            	name: "alfresco/layout/LeftAndRight",
+	                            	config: {
+	                            		widgets: [{
+	                            		        name: "openesdh/common/widgets/lists/CustomPaginator",
+	                            		        align: "right",
+	                            		        id: "HelloWorld"
+	                            		}
+	                            		          
+	                            		          ]
+	                            	}
+		                                
+	                            }
+
+	                        ]
+	                    }
+	                }]
+	            }
+
+
+
+
+	        }]
+	};
