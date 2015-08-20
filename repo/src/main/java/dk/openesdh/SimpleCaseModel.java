e90bda653be4aed9dda9bab7503725e26841829d
Lanre <el_gigantes@hotmail.co.uk>
2015-08-20 13:46:38 +0200

    no message

--- repo/src/main/java/dk/openesdh/SimpleCaseModel.java
+++ repo/src/main/java/dk/openesdh/SimpleCaseModel.java
@@ -6,7 +6,7 @@ import org.alfresco.service.namespace.QName;
  * @author Lanre Abiwon.
  */
 public interface SimpleCaseModel {
-    public static final String SIMPLE_CASE_URI = "http://openesdh.dk/model/case/simple/1.0/";
+    public static final String SIMPLE_CASE_URI = "http://openesdh.dk/model/case/simple/1.0";
     public static final String SIMPLE_CASE_PREFIX = "simple";
     public static final String TYPE_CASE = "case";
 
