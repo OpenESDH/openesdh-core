8cb19552d06e7ec3039334076bfe19547016a105
Seth Yastrov <seth@magenta.dk>
2015-09-09 09:47:32 +0200

    Revert "Remove a parameter to test broken build."
    This reverts commit e3829824f76b840418447f9039f6a8ea272eeae6.

--- openesdh-repo/src/main/java/dk/openesdh/repo/services/cases/CaseServiceImpl.java
+++ openesdh-repo/src/main/java/dk/openesdh/repo/services/cases/CaseServiceImpl.java
@@ -466,7 +466,7 @@ public class CaseServiceImpl implements CaseService, NodeServicePolicies.OnUpdat
      * @return The NodeRef for the folder in which to place the case
      */
     @Override
-    public NodeRef getCaseFolderNodeRef() {
+    public NodeRef getCaseFolderNodeRef(NodeRef casesRootNodeRef) {
         return AuthenticationUtil.runAsSystem(() -> {
             NodeRef casesYearNodeRef = getCasePathNodeRef(casesRootNodeRef, Calendar.YEAR);
             NodeRef casesMonthNodeRef = getCasePathNodeRef(casesYearNodeRef, Calendar.MONTH);
