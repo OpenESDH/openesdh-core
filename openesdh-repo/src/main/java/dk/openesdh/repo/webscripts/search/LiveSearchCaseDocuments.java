360bc80a3f373e5fd703334121d91dcf7a51f803
Lanre Abiwon <lanre@magenta-aps.dk>
2015-09-24 13:25:01 +0200

    1 - Added nodeRefs to the live search return responses

--- openesdh-repo/src/main/java/dk/openesdh/repo/webscripts/search/LiveSearchCaseDocuments.java
+++ openesdh-repo/src/main/java/dk/openesdh/repo/webscripts/search/LiveSearchCaseDocuments.java
@@ -87,10 +87,12 @@ public class LiveSearchCaseDocuments extends DeclarativeWebScript {
             NodeRef docCase = documentService.getCaseNodeRef(document);
             CaseInfo caseItem = caseService.getCaseInfo(docCase);
             //Create the case object which we'll stuff into the document object
+            caseObj.put("caseNodeRef", caseItem.getNodeRef());
             caseObj.put("caseId", caseItem.getCaseId());
             caseObj.put("caseTitle", caseItem.getTitle());
 
             documentObj.put("name", docProps.get(ContentModel.PROP_NAME));
+            documentObj.put("nodeRef", document);
             documentObj.put("type", docProps.get(OpenESDHModel.PROP_DOC_TYPE));
             documentObj.put("docState", docProps.get(OpenESDHModel.PROP_DOC_STATE));
             documentObj.put("docStatus", docProps.get(OpenESDHModel.PROP_OE_STATUS));
