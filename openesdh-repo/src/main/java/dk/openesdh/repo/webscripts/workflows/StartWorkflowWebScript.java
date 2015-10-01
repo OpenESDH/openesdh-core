9a5e012685e49348bf9388801069fd05e546a233
Jurij Rudinskij <Jurij.Rudinskij@visma.com>
2015-10-01 15:58:28 +0300

    StartWorkflowWebScript changed to the one based on annotations.

--- openesdh-repo/src/main/java/dk/openesdh/repo/webscripts/workflows/StartWorkflowWebScript.java
+++ openesdh-repo/src/main/java/dk/openesdh/repo/webscripts/workflows/StartWorkflowWebScript.java
@@ -5,26 +5,36 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.alfresco.service.cmr.workflow.WorkflowPath;
-import org.springframework.extensions.webscripts.AbstractWebScript;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.extensions.webscripts.WebScriptRequest;
 import org.springframework.extensions.webscripts.WebScriptResponse;
+import org.springframework.stereotype.Component;
+
+import com.github.dynamicextensionsalfresco.webscripts.annotations.Attribute;
+import com.github.dynamicextensionsalfresco.webscripts.annotations.HttpMethod;
+import com.github.dynamicextensionsalfresco.webscripts.annotations.Uri;
+import com.github.dynamicextensionsalfresco.webscripts.annotations.WebScript;
 
 import dk.openesdh.repo.model.WorkflowInfo;
 import dk.openesdh.repo.services.workflow.CaseWorkflowService;
 import dk.openesdh.repo.webscripts.utils.WebScriptUtils;
 
-public class StartWorkflowWebScript extends AbstractWebScript {
+@Component
+@WebScript(description = "Starts workflow relevant to the passed data", families = { "Case Workflow Tools" })
+public class StartWorkflowWebScript {
 
+    @Autowired
     private CaseWorkflowService workflowService;
 
-    public void setWorkflowService(CaseWorkflowService workflowService) {
-        this.workflowService = workflowService;
+    @Attribute
+    protected WorkflowInfo getWorkflowInfo(WebScriptRequest req) throws IOException {
+        return (WorkflowInfo) WebScriptUtils.readJson(WorkflowInfo.class, req);
     }
 
-    @Override
-    public void execute(WebScriptRequest req, WebScriptResponse res) throws IOException {
-        WorkflowInfo wfInfo = (WorkflowInfo) WebScriptUtils.readJson(WorkflowInfo.class, req);
-        WorkflowPath wfPath = workflowService.startWorkflow(wfInfo);
+    @Uri(value = "/api/openesdh/workflow/start", method = HttpMethod.POST, defaultFormat = "json")
+    public void startWorkflow(@Attribute WorkflowInfo workflowInfo, WebScriptResponse res)
+            throws IOException {
+        WorkflowPath wfPath = workflowService.startWorkflow(workflowInfo);
         res.setContentEncoding(WebScriptUtils.CONTENT_ENCODING_UTF_8);
         Map<String, String> result = new HashMap<String, String>();
         result.put("workflowPathId", wfPath.getId());
