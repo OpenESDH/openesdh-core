83eff88c13089582462208ab8e739aa19eba9ac7
Jurij Rudinskij <Jurij.Rudinskij@visma.com>
2015-10-01 14:41:41 +0300

    Changed WorkflowTaskDetailsWebScript to the one based on annotations.

--- openesdh-repo/src/main/java/dk/openesdh/repo/webscripts/workflows/WorkflowTaskDetailsWebScript.java
+++ openesdh-repo/src/main/java/dk/openesdh/repo/webscripts/workflows/WorkflowTaskDetailsWebScript.java
@@ -3,29 +3,28 @@ package dk.openesdh.repo.webscripts.workflows;
 import java.io.IOException;
 import java.util.Map;
 
-import org.springframework.extensions.webscripts.AbstractWebScript;
-import org.springframework.extensions.webscripts.WebScriptRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.extensions.webscripts.WebScriptResponse;
-import org.springframework.util.StringUtils;
+import org.springframework.stereotype.Component;
+
+import com.github.dynamicextensionsalfresco.webscripts.annotations.HttpMethod;
+import com.github.dynamicextensionsalfresco.webscripts.annotations.Uri;
+import com.github.dynamicextensionsalfresco.webscripts.annotations.UriVariable;
+import com.github.dynamicextensionsalfresco.webscripts.annotations.WebScript;
 
 import dk.openesdh.repo.services.workflow.WorkflowTaskService;
 import dk.openesdh.repo.webscripts.utils.WebScriptUtils;
 
-public class WorkflowTaskDetailsWebScript extends AbstractWebScript {
+@Component
+@WebScript(description = "Retrieves details of the provided workflow task.", families = { "Case Workflow Tools" })
+public class WorkflowTaskDetailsWebScript {
 
+    @Autowired
     private WorkflowTaskService workflowTaskService;
 
-    public void setWorkflowTaskService(WorkflowTaskService workflowTaskService) {
-        this.workflowTaskService = workflowTaskService;
-    }
-
-    @Override
-    public void execute(WebScriptRequest req, WebScriptResponse res) throws IOException {
-        Map<String, String> templateArgs = req.getServiceMatch().getTemplateVars();
-        String taskId = templateArgs.get(WebScriptUtils.TASK_ID);
-        if (StringUtils.isEmpty(taskId)) {
-            return;
-        }
+    @Uri(value = "/api/openesdh/workflow/task/{taskId}/details", method = HttpMethod.GET, defaultFormat = "json")
+    public void getWorkflowTaskDetails(@UriVariable(value = "taskId") final String taskId, WebScriptResponse res)
+            throws IOException {
         Map<String, Object> task = workflowTaskService.getWorkflowTask(taskId);
         res.setContentEncoding(WebScriptUtils.CONTENT_ENCODING_UTF_8);
         WebScriptUtils.writeJson(task, res);
