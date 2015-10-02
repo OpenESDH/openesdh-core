eada69250d05ba14249871ed8e295d12bfa4c5e0
Jurij Rudinskij <Jurij.Rudinskij@visma.com>
2015-10-02 08:06:13 +0300

    Several refactorings for workflow Web Scripts to enhance readability.

--- openesdh-repo/src/main/java/dk/openesdh/repo/webscripts/workflows/StartWorkflowWebScript.java
+++ openesdh-repo/src/main/java/dk/openesdh/repo/webscripts/workflows/StartWorkflowWebScript.java
@@ -7,13 +7,13 @@ import java.util.Map;
 import org.alfresco.service.cmr.workflow.WorkflowPath;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.extensions.webscripts.WebScriptRequest;
-import org.springframework.extensions.webscripts.WebScriptResponse;
 import org.springframework.stereotype.Component;
 
 import com.github.dynamicextensionsalfresco.webscripts.annotations.Attribute;
 import com.github.dynamicextensionsalfresco.webscripts.annotations.HttpMethod;
 import com.github.dynamicextensionsalfresco.webscripts.annotations.Uri;
 import com.github.dynamicextensionsalfresco.webscripts.annotations.WebScript;
+import com.github.dynamicextensionsalfresco.webscripts.resolutions.Resolution;
 
 import dk.openesdh.repo.model.WorkflowInfo;
 import dk.openesdh.repo.services.workflow.CaseWorkflowService;
@@ -32,13 +32,11 @@ public class StartWorkflowWebScript {
     }
 
     @Uri(value = "/api/openesdh/workflow/start", method = HttpMethod.POST, defaultFormat = "json")
-    public void startWorkflow(@Attribute WorkflowInfo workflowInfo, WebScriptResponse res)
-            throws IOException {
+    public Resolution startWorkflow(@Attribute WorkflowInfo workflowInfo) {
         WorkflowPath wfPath = workflowService.startWorkflow(workflowInfo);
-        res.setContentEncoding(WebScriptUtils.CONTENT_ENCODING_UTF_8);
         Map<String, String> result = new HashMap<String, String>();
         result.put("workflowPathId", wfPath.getId());
-        WebScriptUtils.writeJson(result, res);
+        return WebScriptUtils.jsonResolution(result);
     }
 
 }
--- openesdh-repo/src/main/java/dk/openesdh/repo/webscripts/workflows/WorkflowTaskDetailsWebScript.java
+++ openesdh-repo/src/main/java/dk/openesdh/repo/webscripts/workflows/WorkflowTaskDetailsWebScript.java
@@ -1,16 +1,15 @@
 package dk.openesdh.repo.webscripts.workflows;
 
-import java.io.IOException;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.extensions.webscripts.WebScriptResponse;
 import org.springframework.stereotype.Component;
 
 import com.github.dynamicextensionsalfresco.webscripts.annotations.HttpMethod;
 import com.github.dynamicextensionsalfresco.webscripts.annotations.Uri;
 import com.github.dynamicextensionsalfresco.webscripts.annotations.UriVariable;
 import com.github.dynamicextensionsalfresco.webscripts.annotations.WebScript;
+import com.github.dynamicextensionsalfresco.webscripts.resolutions.Resolution;
 
 import dk.openesdh.repo.services.workflow.WorkflowTaskService;
 import dk.openesdh.repo.webscripts.utils.WebScriptUtils;
@@ -23,11 +22,9 @@ public class WorkflowTaskDetailsWebScript {
     private WorkflowTaskService workflowTaskService;
 
     @Uri(value = "/api/openesdh/workflow/task/{taskId}/details", method = HttpMethod.GET, defaultFormat = "json")
-    public void getWorkflowTaskDetails(@UriVariable(value = "taskId") final String taskId, WebScriptResponse res)
-            throws IOException {
+    public Resolution getWorkflowTaskDetails(@UriVariable(WebScriptUtils.TASK_ID) final String taskId) {
         Map<String, Object> task = workflowTaskService.getWorkflowTask(taskId);
-        res.setContentEncoding(WebScriptUtils.CONTENT_ENCODING_UTF_8);
-        WebScriptUtils.writeJson(task, res);
+        return WebScriptUtils.jsonResolution(task);
     }
 
 }
