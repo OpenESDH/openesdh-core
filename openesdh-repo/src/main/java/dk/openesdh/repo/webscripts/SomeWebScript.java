a997cc76a1e08cc137744c1ba1289b349730d411
Jurij Rudinskij <Jurij.Rudinskij@visma.com>
2015-10-30 16:59:08 +0200

    KLE-SYNC Manual kle synchronizer issue web script.

--- openesdh-repo/src/main/java/dk/openesdh/repo/webscripts/SomeWebScript.java
+++ openesdh-repo/src/main/java/dk/openesdh/repo/webscripts/SomeWebScript.java
@@ -1,23 +1,28 @@
 package dk.openesdh.repo.webscripts;
 
-import java.io.IOException;
-
-import org.springframework.extensions.webscripts.WebScriptResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import com.github.dynamicextensionsalfresco.webscripts.annotations.HttpMethod;
-import com.github.dynamicextensionsalfresco.webscripts.annotations.RequestParam;
 import com.github.dynamicextensionsalfresco.webscripts.annotations.Uri;
 import com.github.dynamicextensionsalfresco.webscripts.annotations.WebScript;
+import com.github.dynamicextensionsalfresco.webscripts.resolutions.Resolution;
+
+import dk.openesdh.repo.classification.sync.kle.KLEClassificationSynchronizer;
+import dk.openesdh.repo.webscripts.utils.WebScriptUtils;
 
 @Component
 @WebScript(families = { "somesome" }, description = "some description", defaultFormat = "json")
 public class SomeWebScript {
 
-    @Uri(value = "/dynamic-extensions/examples/some", method = HttpMethod.GET)
-    public void doIt(@RequestParam(required = false) final String param, final WebScriptResponse response)
-            throws IOException {
-        System.out.println("This is param: " + param);
-        response.getWriter().write("hello world");
+    @Autowired
+    @Qualifier("kleClassificationSynchronizer")
+    private KLEClassificationSynchronizer kleSynchronizer;
+
+    @Uri(value = "/kle", method = HttpMethod.GET)
+    public Resolution doIt() {
+        kleSynchronizer.synchronize();
+        return WebScriptUtils.jsonResolution("done");
     }
 }
