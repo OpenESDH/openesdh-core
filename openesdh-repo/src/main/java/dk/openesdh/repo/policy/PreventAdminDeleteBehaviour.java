465bddd83b37a07a4d67af622eba9b929ff46b9c
Lanre Abiwon <lanre@magenta-aps.dk>
2015-11-19 11:01:10 +0100

    1 - Prevent the deletion of the admin user account

---
+++ openesdh-repo/src/main/java/dk/openesdh/repo/policy/PreventAdminDeleteBehaviour.java
@@ -0,0 +1,60 @@
+package dk.openesdh.repo.policy;
+
+import org.alfresco.error.AlfrescoRuntimeException;
+import org.alfresco.model.ContentModel;
+import org.alfresco.repo.node.NodeServicePolicies;
+import org.alfresco.repo.policy.Behaviour;
+import org.alfresco.repo.policy.JavaBehaviour;
+import org.alfresco.repo.policy.PolicyComponent;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.security.PersonService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Guard against the deletion of the admin user.
+ *
+ * @author Lanre
+ */
+@Service("beforeDeletePersonPolicy")
+public class PreventAdminDeleteBehaviour implements NodeServicePolicies.BeforeDeleteNodePolicy {
+    private static Log logger = LogFactory.getLog(PreventAdminDeleteBehaviour.class);
+
+    //<editor-fold desc="Dependencies">
+    @Autowired
+    @Qualifier("PersonService")
+    private PersonService personService;
+    @Autowired
+    @Qualifier("policyComponent")
+    private PolicyComponent policyComponent;
+    //</editor-fold>
+
+    @PostConstruct
+    public void init() {
+
+        // Create behaviours
+        Behaviour beforeDeleteNode = new JavaBehaviour(this, "beforeDeleteNode", Behaviour.NotificationFrequency.TRANSACTION_COMMIT);
+
+        // Bind behaviours to node policies
+        this.policyComponent.bindClassBehaviour(NodeServicePolicies.BeforeDeleteNodePolicy.QNAME, ContentModel.TYPE_PERSON,
+                beforeDeleteNode);
+    }
+
+    /**
+     * Called before a node is deleted and checks if the user name of the person being deleted is "admin". If so a
+     * Runtime Exception is thrown.
+     *
+     * @param nodeRef the node reference
+     */
+    @Override
+    public void beforeDeleteNode(NodeRef nodeRef) {
+        PersonService.PersonInfo person = personService.getPerson(nodeRef);
+        if(person.getUserName().equalsIgnoreCase("admin"))
+            throw new AlfrescoRuntimeException("Deletion of primary admin user is not allowed.");
+    }
+}
