ce292c6557c9f4501eeeb5addfcd33aeb4665280
Arnas Petraitis <arnas.petraitis@visma.com>
2015-12-28 13:23:42 +0200

    annotated document categories webscript

--- openesdh-repo/src/main/java/dk/openesdh/repo/webscripts/documents/DocumentCategoriesWebScript.java
+++ openesdh-repo/src/main/java/dk/openesdh/repo/webscripts/documents/DocumentCategoriesWebScript.java
@@ -1,58 +1,53 @@
 package dk.openesdh.repo.webscripts.documents;
 
-import dk.openesdh.repo.model.DocumentCategory;
-import dk.openesdh.repo.services.documents.DocumentCategoryService;
-import dk.openesdh.repo.services.system.MultiLanguageValue;
-import dk.openesdh.repo.webscripts.AbstractRESTWebscript;
-import dk.openesdh.repo.webscripts.ParamUtils;
-import static dk.openesdh.repo.webscripts.ParamUtils.checkRequiredParam;
-import dk.openesdh.repo.webscripts.utils.WebScriptUtils;
+import com.github.dynamicextensionsalfresco.webscripts.annotations.Authentication;
+import com.github.dynamicextensionsalfresco.webscripts.annotations.AuthenticationType;
+import com.github.dynamicextensionsalfresco.webscripts.annotations.HttpMethod;
+import com.github.dynamicextensionsalfresco.webscripts.annotations.RequestParam;
+import com.github.dynamicextensionsalfresco.webscripts.annotations.Uri;
+import com.github.dynamicextensionsalfresco.webscripts.annotations.WebScript;
+import com.github.dynamicextensionsalfresco.webscripts.resolutions.Resolution;
+
 import java.io.IOException;
-import java.util.Map;
 import java.util.stream.Collectors;
+
 import org.alfresco.service.cmr.repository.NodeRef;
-import org.codehaus.plexus.util.StringUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.extensions.webscripts.WebScriptException;
-import org.springframework.extensions.webscripts.WebScriptRequest;
-import org.springframework.extensions.webscripts.WebScriptResponse;
+import org.springframework.stereotype.Component;
 
-public class DocumentCategoriesWebScript extends AbstractRESTWebscript {
+import dk.openesdh.repo.model.DocumentCategory;
+import dk.openesdh.repo.services.documents.DocumentCategoryService;
+import dk.openesdh.repo.services.system.MultiLanguageValue;
+import dk.openesdh.repo.webscripts.utils.WebScriptUtils;
 
+@Component
+@WebScript(description = "Manage document categories", families = {"Case Document Tools"})
+public class DocumentCategoriesWebScript {
+
+    @Autowired
     private DocumentCategoryService documentCategoryService;
 
-    public void setDocumentCategoryService(DocumentCategoryService documentCategoryService) {
-        this.documentCategoryService = documentCategoryService;
-    }
-
-    @Override
-    protected NodeRef getNodeRef(WebScriptRequest req, Map<String, String> templateArgs) {
-        String nodeRefId = req.getParameter("nodeRefId");
-        if (StringUtils.isNotEmpty(nodeRefId)) {
-            return new NodeRef(nodeRefId);
-        }
-        return super.getNodeRef(req, templateArgs);
-    }
-
-
-    @Override
-    protected void get(NodeRef nodeRef, WebScriptRequest req, WebScriptResponse res) throws IOException, JSONException {
-        res.setContentEncoding(WebScriptUtils.CONTENT_ENCODING_UTF_8);
-        if (nodeRef == null) {
-            new JSONArray(documentCategoryService.getDocumentCategories().stream()
-                    .map(DocumentCategory::toJSONObject)
-                    .collect(Collectors.toList())
-            ).write(res.getWriter());
-            return;
-        }
-        DocumentCategory documentCategory = documentCategoryService.getDocumentCategory(nodeRef);
+    @Authentication(AuthenticationType.ADMIN)
+    @Uri(value = "/api/openesdh/document/category?nodeRefId=", method = HttpMethod.GET, defaultFormat = "json")
+    public Resolution get(@RequestParam(required = true) final NodeRef nodeRefId) throws JSONException {
+        DocumentCategory documentCategory = documentCategoryService.getDocumentCategory(nodeRefId);
         if (documentCategory == null) {
             throw new WebScriptException("Document category not found");
         }
-        createJSONObjectWithMultilanguage(documentCategory)
-                .write(res.getWriter());
+        return WebScriptUtils.jsonResolution(createJSONObjectWithMultilanguage(documentCategory));
+    }
+
+    @Uri(value = "/api/openesdh/document/categories", method = HttpMethod.GET, defaultFormat = "json")
+    public Resolution getCategories() throws IOException, JSONException {
+        return WebScriptUtils.jsonResolution(
+                new JSONArray(documentCategoryService.getDocumentCategories().stream()
+                        .map(DocumentCategory::toJSONObject)
+                        .collect(Collectors.toList())
+                ));
     }
 
     private JSONObject createJSONObjectWithMultilanguage(DocumentCategory documentCategory) throws JSONException {
@@ -61,23 +56,24 @@ public class DocumentCategoriesWebScript extends AbstractRESTWebscript {
         return json;
     }
 
-    @Override
-    protected void post(NodeRef nodeRef, WebScriptRequest req, WebScriptResponse res) throws IOException, JSONException {
-        String name = ParamUtils.getRequiredParameter(req, "name");
-        String mlDisplayNames = ParamUtils.getRequiredParameter(req, "mlDisplayNames");
-        DocumentCategory savedDocumentCategory = createOrUpdateDocumentCategory(nodeRef, name, mlDisplayNames);
-        res.setContentEncoding(WebScriptUtils.CONTENT_ENCODING_UTF_8);
-        savedDocumentCategory.toJSONObject()
-                .write(res.getWriter());
+    @Authentication(AuthenticationType.ADMIN)
+    @Uri(value = "/api/openesdh/document/category?nodeRefId=", method = HttpMethod.POST, defaultFormat = "json")
+    public Resolution post(
+            @RequestParam(required = false) final NodeRef nodeRefId,
+            @RequestParam(required = true) final String name,
+            @RequestParam(required = true) final String mlDisplayNames
+    ) throws IOException, JSONException {
+        DocumentCategory savedDocumentCategory = createOrUpdateDocumentCategory(nodeRefId, name, mlDisplayNames);
+        return WebScriptUtils.jsonResolution(savedDocumentCategory.toJSONObject());
     }
 
-    @Override
-    protected void delete(NodeRef nodeRef, WebScriptRequest req, WebScriptResponse res) throws IOException, JSONException {
-        checkRequiredParam(nodeRef, "nodeRef");
+    @Authentication(AuthenticationType.ADMIN)
+    @Uri(value = "/api/openesdh/document/category?nodeRefId=", method = HttpMethod.DELETE, defaultFormat = "json")
+    public Resolution delete(@RequestParam(required = true) final NodeRef nodeRefId) throws IOException, JSONException {
         DocumentCategory category = new DocumentCategory();
-        category.setNodeRef(nodeRef);
+        category.setNodeRef(nodeRefId);
         documentCategoryService.deleteDocumentCategory(category);
-        res.getWriter().append("Deleted succesfully");
+        return WebScriptUtils.jsonResolution("Deleted succesfully");
     }
 
     private DocumentCategory createOrUpdateDocumentCategory(NodeRef nodeRef, String name, String mlDisplayNames) throws JSONException {
