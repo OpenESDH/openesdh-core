cca1c42d875f9bfdd6362f3eaf5bfa5b71298a0c
Arnas Petraitis <arnas.petraitis@visma.com>
2016-01-22 13:26:20 +0200

    OPENE-432: oeFile comments

--- openesdh-repo/src/test/java/dk/openesdh/repo/services/files/OeFilesServiceImplIT.java
+++ openesdh-repo/src/test/java/dk/openesdh/repo/services/files/OeFilesServiceImplIT.java
@@ -1,16 +1,18 @@
 package dk.openesdh.repo.services.files;
 
-import com.tradeshift.test.remote.Remote;
-import com.tradeshift.test.remote.RemoteTestRunner;
+import static org.alfresco.repo.security.authentication.AuthenticationUtil.setAdminUserAsFullyAuthenticatedUser;
+import static org.alfresco.repo.security.authentication.AuthenticationUtil.setFullyAuthenticatedUser;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.Date;
 import java.util.List;
 
+import org.alfresco.model.ContentModel;
 import org.alfresco.repo.content.MimetypeMap;
-import static org.alfresco.repo.security.authentication.AuthenticationUtil.setAdminUserAsFullyAuthenticatedUser;
-import static org.alfresco.repo.security.authentication.AuthenticationUtil.setFullyAuthenticatedUser;
 import org.alfresco.repo.security.authority.script.ScriptAuthorityService;
 import org.alfresco.repo.security.authority.script.ScriptGroup;
 import org.alfresco.repo.security.permissions.AccessDeniedException;
@@ -19,26 +21,30 @@ import org.alfresco.service.cmr.repository.NodeService;
 import org.alfresco.service.cmr.security.AuthorityService;
 import org.alfresco.service.cmr.security.PermissionService;
 import org.alfresco.service.transaction.TransactionService;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.junit.After;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.tradeshift.test.remote.Remote;
+import com.tradeshift.test.remote.RemoteTestRunner;
+
 import dk.openesdh.repo.helper.CaseHelper;
 import dk.openesdh.repo.services.RunInTransactionAsAdmin;
-
-import static org.alfresco.repo.security.authentication.AuthenticationUtil.setFullyAuthenticatedUser;
+import dk.openesdh.repo.services.system.OpenESDHFoldersService;
 
 @RunWith(RemoteTestRunner.class)
 @Remote(runnerClass = SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:alfresco/application-context.xml", "classpath:alfresco/extension/openesdh-test-context.xml"})
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class OeFilesServiceImplIT implements RunInTransactionAsAdmin {
 
     @Autowired
@@ -58,6 +64,8 @@ public class OeFilesServiceImplIT implements RunInTransactionAsAdmin {
     @Autowired
     @Qualifier("NodeService")
     private NodeService nodeService;
+    @Autowired
+    private OpenESDHFoldersService openESDHFoldersService;
 
     private static final String USER_OWNER1 = "fileTestOwner1";
     private static final String USER_OWNER2 = "fileTestOwner2";
@@ -66,14 +74,16 @@ public class OeFilesServiceImplIT implements RunInTransactionAsAdmin {
     private NodeRef file1;
     private NodeRef owner1;
     private NodeRef owner2;
+    private String testComment;
 
     @Before
     public void setUp() {
         setAdminUserAsFullyAuthenticatedUser();
         owner1 = caseHelper.createDummyUser(USER_OWNER1);
         owner2 = caseHelper.createDummyUser(USER_OWNER2);
-
-        file1 = filesService.addFile(owner1, "test_file_" + new Date().getTime() + ".txt", MimetypeMap.MIMETYPE_TEXT_PLAIN, fileBytes);
+        long timestamp = new Date().getTime();
+        testComment = "Test Comment On file " + timestamp;
+        file1 = filesService.addFile(owner1, "test_file_" + timestamp + ".txt", MimetypeMap.MIMETYPE_TEXT_PLAIN, fileBytes, testComment);
     }
 
     @After
@@ -93,7 +103,16 @@ public class OeFilesServiceImplIT implements RunInTransactionAsAdmin {
     }
 
     @Test
-    public void owner1_gets_his_file() {
+    public void a_file_is_commented() {
+        setFullyAuthenticatedUser(USER_OWNER1);
+        JSONObject file = filesService.getFile(file1);
+        JSONArray comments = (JSONArray) file.get("comments");
+        assertEquals("file has 1 comment", 1, comments.size());
+        assertEquals("comment is correct", testComment, ((JSONObject) comments.get(0)).get("comment"));
+    }
+
+    @Test
+    public void owner1_gets_his_files() {
         setFullyAuthenticatedUser(USER_OWNER1);
         List<JSONObject> files = filesService.getFiles(USER_OWNER1);
         assertEquals(USER_OWNER1 + " has one file", 1, files.size());
@@ -103,6 +122,18 @@ public class OeFilesServiceImplIT implements RunInTransactionAsAdmin {
     public void owner2_doesnt_get_owner1s_files() {
         setFullyAuthenticatedUser(USER_OWNER2);
         filesService.getFiles(USER_OWNER1);
+    }
+
+    @Test
+    public void owner1_gets_his_file_by_id() {
+        setFullyAuthenticatedUser(USER_OWNER1);
+        assertNotNull(USER_OWNER1 + " gets his file by id", filesService.getFile(file1));
+    }
+
+    @Test(expected = AccessDeniedException.class)
+    public void owner2_doesnt_get_owner1s_file() {
+        setFullyAuthenticatedUser(USER_OWNER2);
+        filesService.getFile(file1);
     }
 
     public void testAddFile() {
@@ -157,6 +188,34 @@ public class OeFilesServiceImplIT implements RunInTransactionAsAdmin {
             });
         }
     }
+
+    @Test
+    public void owner1_can_access_his_folder_and_file() {
+        setFullyAuthenticatedUser(USER_OWNER1);
+        //check access to files folder ALLOWED
+        NodeRef filesFolder = openESDHFoldersService.getFilesRootNodeRef();
+        //check access to user files folder ALLOWED
+        NodeRef userFilesFolder = nodeService.getChildByName(filesFolder, ContentModel.ASSOC_CONTAINS, USER_OWNER1);
+        //check access to file ALLOWED
+        nodeService.getProperty(file1, ContentModel.PROP_NAME);
+    }
+
+    @Test(expected = AccessDeniedException.class)
+    public void owner2_cant_access_owner1s_folder() {
+        setFullyAuthenticatedUser(USER_OWNER2);
+        //check access to files folder ALLOWED
+        NodeRef filesFolder = openESDHFoldersService.getFilesRootNodeRef();
+        //check access to user files folder DENIED
+        nodeService.getChildByName(filesFolder, ContentModel.ASSOC_CONTAINS, USER_OWNER1);
+    }
+
+    @Test(expected = AccessDeniedException.class)
+    public void owner2_cant_access_owner1s_file() {
+        setFullyAuthenticatedUser(USER_OWNER2);
+        //check access to file DENIED
+        nodeService.getProperty(file1, ContentModel.PROP_NAME);
+    }
+
 
     @Override
     public TransactionService getTransactionService() {
