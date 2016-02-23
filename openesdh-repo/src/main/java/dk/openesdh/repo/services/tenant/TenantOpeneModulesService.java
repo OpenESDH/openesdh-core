df28484c4cb0f8734908728e935e944659dcc5c3
Arnas Petraitis <arnas.petraitis@visma.com>
2016-02-23 15:33:48 +0200

    show tenants with empty list of modules

--- openesdh-repo/src/main/java/dk/openesdh/repo/services/tenant/TenantOpeneModulesService.java
+++ openesdh-repo/src/main/java/dk/openesdh/repo/services/tenant/TenantOpeneModulesService.java
@@ -1,53 +1,62 @@
 package dk.openesdh.repo.services.tenant;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
-import org.alfresco.service.cmr.repository.ContentIOException;
 import org.alfresco.service.namespace.QName;
-import org.json.JSONException;
-
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 
 public interface TenantOpeneModulesService {
 
     /**
      * Retrieves a map of all tenants with enabled modules. The map doesn't
      * contain tenants with no modules enabled.
+     *
+     * @return
      */
-    Map<String, List<String>> getAllTenantsModules() throws JsonParseException, JsonMappingException, IOException;
+    Map<String, List<String>> getAllTenantsModules();
 
     /**
      * Retrieves list of modules of the specified tenant.
+     *
+     * @param tenant
+     * @return
      */
-    List<String> getTenantModules(String tenant) throws JsonParseException, JsonMappingException, IOException;
+    List<String> getTenantModules(String tenant);
 
     /**
      * Saves enabled modules list for the specified tenant.
+     *
+     * @param tenant
+     * @param modules
      */
-    void saveTenantModules(String tenant, List<String> modules) throws JsonParseException, JsonMappingException,
-            IOException, ContentIOException, JSONException;
+    void saveTenantModules(String tenant, List<String> modules);
 
     /**
      * Retrieves a list of OpenE modules which can be enabled for tenants.
+     *
+     * @return
      */
     List<String> getOpeneModules();
 
     /**
      * Removes enabled modules configuration for the specified tenant.
+     *
+     * @param tenant
      */
-    void deleteTenantModules(String tenant) throws JsonParseException, JsonMappingException, IOException;
+    void deleteTenantModules(String tenant);
 
     /**
      * Checks whether the module for the specified case type is enabled for the
      * current tenant.
+     *
+     * @param caseType
      */
     void checkCaseTypeModuleEnabled(QName caseType);
 
     /**
      * Checks whether the specified module is enabled for the current tenant.
+     *
+     * @param module
      */
     void checkCurrentTenantModuleEnabled(String module);
 }
