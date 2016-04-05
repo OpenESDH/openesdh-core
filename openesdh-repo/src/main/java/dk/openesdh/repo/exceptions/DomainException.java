a0feca3525af10a06d685eac5072944087f489f4
Arnas Petraitis <arnas.petraitis@visma.com>
2016-04-05 15:40:27 +0300

    OPENE-431 some customization on DomainException

--- openesdh-repo/src/main/java/dk/openesdh/repo/exceptions/DomainException.java
+++ openesdh-repo/src/main/java/dk/openesdh/repo/exceptions/DomainException.java
@@ -1,10 +1,14 @@
 package dk.openesdh.repo.exceptions;
 
+import org.json.JSONObject;
+
 public class DomainException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
     private final boolean domain = true;
+
+    private final JSONObject props;
 
     /**
      * exception is handled in front-end
@@ -12,7 +16,7 @@ public class DomainException extends RuntimeException {
      * @param errorCode - code will be used as translate code in front-end
      */
     public DomainException(String errorCode) {
-        super(errorCode);
+        this(errorCode, (JSONObject) null);
     }
 
     /**
@@ -22,7 +26,30 @@ public class DomainException extends RuntimeException {
      * @param cause
      */
     public DomainException(String errorCode, Throwable cause) {
+        this(errorCode, cause, null);
+    }
+
+    /**
+     * exception is handled in front-end
+     *
+     * @param errorCode - code will be used as translate code in front-end
+     * @param props - json will be added to response
+     */
+    public DomainException(String errorCode, JSONObject props) {
+        super(errorCode);
+        this.props = props;
+    }
+
+    /**
+     * exception is handled in front-end
+     *
+     * @param errorCode - code will be used as translate code in front-end
+     * @param cause
+     * @param props - json will be added to response
+     */
+    public DomainException(String errorCode, Throwable cause, JSONObject props) {
         super(errorCode, cause);
+        this.props = props;
     }
 
     public boolean isDomain() {
@@ -31,5 +58,9 @@ public class DomainException extends RuntimeException {
 
     public String getErrorCode() {
         return getMessage();
+    }
+
+    public JSONObject getProps() {
+        return props;
     }
 }
